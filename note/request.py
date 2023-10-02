ac239f29f36fc70f59503f3b46155ea8690b0e61
jona1167 <68178205+jona1167@users.noreply.github.com>
2023-10-02 13:22:30 +0800

    Update request.py

--- note/request.py
+++ note/request.py
@@ -5,7 +5,7 @@ url = "https://doctorgpt.p.rapidapi.com/doctorgpt"
 querystring = {"condition":"diabetes","specialist":"true"}
 
 headers = {
-	"X-RapidAPI-Key": "49e3e5866fmshe7319c653c8a4fdp1d1721jsne467a20e9e73",
+	"X-RapidAPI-Key": "",
 	"X-RapidAPI-Host": "doctorgpt.p.rapidapi.com"
 }
 
